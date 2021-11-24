f6d0a4fd56dc3d40c2e1a5593e49a99c6ad338b9
septialeyda <septialeyda@apps.ipb.ac.id>
2021-11-24 11:55:39 +0700

    ganti bg login register

--- apl (2).py
+++ apl (2).py
@@ -11,7 +11,7 @@ app = Flask(__name__, static_folder=STATIC_FOLDER)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'floads'
+app.config['MYSQL_DB'] = 'floads(2)'
 
 mysql = MySQL(app)
 
